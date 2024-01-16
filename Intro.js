4b54e66bce7291905989a385a1182b8ea3196383
abhishekpatra <abhishekpatra6129@gmail.com>
2024-01-16 15:01:48 +0530

    n1

---
+++ Intro.js
@@ -0,0 +1,8 @@
+// Javascript is a high level object-oriented  multi paradigm programming  language
+//Instructs computer to do thingd,we dont have to worry about memory management as it is a high level language,
+//based on objects for storing different kinds of data
+//we can use imperative and declarative programming as it is a multi paradigm language
+
+//content--HTML(NOUNS)
+//presentation---CSS(ADJECTIVES)
+//programming language----JAVASCRIPT(VERBS)
